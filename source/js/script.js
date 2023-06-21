32aa01b5b6355096026eb33b348e25122918c673
Мария <marklukanova@gmail.com>
2023-06-22 02:48:27 +0300

    feat: add swiper

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,18 @@
 /* в этот файл добавляет скрипты*/
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js';
+
+const swiper = new Swiper(".swiper", {
+  grabCursor: 'true',
+  navigation: {
+    nextEl: ".swiper__button--next",
+    prevEl: ".swiper__button--prev",
+  },
+  pagination: {
+    el: ".swiper__pagination",
+  },
+  mousewheel: false,
+  keyboard: true,
+
+  wrapperClass: 'swiper__wrapper',
+  slideClass: 'swiper__item',
+});
