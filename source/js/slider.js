d6647bd3cd837d346a45940aa164414914d0bb50
Мария <marklukanova@gmail.com>
2023-06-25 20:16:33 +0300

    fix: add fixes

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,6 +1,8 @@
 import '../vendor/nouislider/nouislider.js';
 
 const slider = document.querySelector('.slider__range');
+const minField = document.querySelector('.slider__input--min');
+const maxField = document.querySelector('.slider__input--max');
 
 const createSlider = () => {
   noUiSlider.create(slider, {
@@ -8,10 +10,16 @@ const createSlider = () => {
       min: 0,
       max: 123,
     },
-    start: [3, 106],
+    start: [0, 123],
+    step: 1,
     connect: true,
 
     cssPrefix: 'slider__',
+  });
+
+  slider.noUiSlider.on('update', () => {
+    minField.value = parseInt(slider.noUiSlider.get()[0]);
+    maxField.value = parseInt(slider.noUiSlider.get()[1]);
   });
 }
 
