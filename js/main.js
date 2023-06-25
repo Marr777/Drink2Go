4fe6b3ce582f0a09f1c197fd87f6f2bb4aee96d2
Мария <marklukanova@gmail.com>
2023-06-25 20:54:16 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,1 @@
+import{setSwiper}from"./swiper.js";import{createSlider}from"./slider.js";import{checkWindowSize}from"./pagination.js";import{setMap}from"./map.js";import{initBurgerMenu}from"./burger.js";initBurgerMenu(),setSwiper(),createSlider(),checkWindowSize(),setMap();
